a7d0a0506903a614c33f1d5e022e5ac6277162ce
Zemheri25 <omerzemheri14@gmail.com>
2021-11-08 14:56:38 +0300

    Bu benim ilk çalışmam

---
+++ ilk proje.py
@@ -0,0 +1,9 @@
+	
+password = "W@12"
+name ="Joseph"
+your_name = input("Plesae enter your name : ")
+
+if your_name == name:
+    print(F"Hello, {your_name}!The password is : {password}.")
+else:
+    print(f"Hello {your_name}!See you later.")
