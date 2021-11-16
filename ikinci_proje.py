3556d98a4a93035f0c3c83e1fa76749f5e1e63f5
Zemheri25 <omerzemheri14@gmail.com>
2021-11-16 23:04:48 +0300

    this is my second project

---
+++ ikinci_proje.py
@@ -0,0 +1,8 @@
+	
+age = input("Are you a cigarette addict older than 75 years old?")
+chronic = input("Do you have a severe chronic disease ? ")
+immune = input("İS your immune system too weak? ")
+if age == True or chronic == True or immune == True:
+    print("You are in risky group!")
+else:
+    print("You are not in risky group!") 
