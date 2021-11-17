d770c0ae075ef4716020481f2c5645153d5b51f5
Zemheri25 <omerzemheri14@gmail.com>
2021-11-17 20:08:15 +0300

    armstrong number

--- armstrong.py
+++ armstrong.py
@@ -1,6 +1,6 @@
 count = 0
 number =input("Please enter your number ? ")
-if number.isnumeric():
+if number.isnumeric() and int(number) > 0:
     for i in number:
        i = int(i)
        count = count + i**len(number)
@@ -8,12 +8,6 @@ if number.isnumeric():
         print(f"{number} is an Armstrong number.")
     else:
         print(f"{number} is not an Armstrong number")  
-
- 
-elif number.isalpha():
-    print("It is an invalid entry.Don't use non-numeric,float or negative values!")
-elif int(number) < 0:
-    print("It is an invalid entry.Don't use non-numeric,float or negative values!")
 else:
     print("It is an invalid entry.Don't use non-numeric,float or negative values!")
 
