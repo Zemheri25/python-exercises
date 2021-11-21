f7638e6a253358da5f4c9ed2b8bffe70b35e4e60
Zemheri25 <omerzemheri14@gmail.com>
2021-11-21 20:06:30 +0300

    I have changed and added somthing

--- prime_number.py
+++ prime_number.py
@@ -7,3 +7,18 @@ if len(list1) > 2:
     print(f"Your number {number} is not a prime number ")
 else:
     print(f"Your number {number} is a prime number ")
+
+
+
+def primenumber(x):
+    list1 = []
+    for i in range(1, x+1):
+        if x % i == 0:
+            list1.append(i)
+    if len(list1) > 2:                                          #THİS SOLUTİON WİTH PYTHON FUNCTİON
+        return (f"Your number {x} is not prime number. ")
+    else:
+        return (f"Your number {x} is prime number.")
+
+
+print(primenumber(4))
