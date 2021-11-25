72f5312e235b07b3acea66fec369a752a4e792c0
Zemheri25 <omerzemheri14@gmail.com>
2021-11-26 00:46:15 +0300

    prime number2 assigment

---
+++ prime_number2.py
@@ -0,0 +1,27 @@
+list1 = []
+for i in range(100):
+    count = 0
+    for ii in range(1, i+1):
+        if i % ii == 0:
+           count += 1
+    if count == 2:
+        list1.append(i)
+
+
+print(list1)
+
+
+
+
+def prime_number(range1):
+    list1 = []
+    for i in range(range1):
+        count = 0
+        for ii in range(1, i+1):
+            if i % ii == 0:
+                count += 1
+        if count == 2:
+            list1.append(i)
+    return list1
+
+print(prime_number(int(input("Please enter your number : "))))
