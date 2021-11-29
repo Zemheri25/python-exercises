9e63deff4701e509d27ee031324ad6e2ebc70672
Zemheri25 <omerzemheri14@gmail.com>
2021-11-29 15:27:40 +0300

    I have added new solution with list comprehension

--- prime_number2.py
+++ prime_number2.py
@@ -25,3 +25,16 @@ def prime_number(range1):
     return list1
 
 print(prime_number(int(input("Please enter your number : "))))
+
+
+
+
+
+
+list1 = []
+number = 20
+for i in range(2, number+1):
+    a = [ii for ii in range(1, i+1) if i % ii == 0]
+    if len(a) == 2:
+        list1.append(i)
+print(list1)
