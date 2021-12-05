6b5a750b3914b84cc4c1f4c54b5ea0d72babad77
Zemheri25 <omerzemheri14@gmail.com>
2021-12-05 17:05:10 +0300

    conrolling the result of calculator operation

---
+++ controlling.py
@@ -0,0 +1,29 @@
+string1 = ("6 * 4 = 24").split()
+
+if string1[1] == "*":
+    a = (int(string1[0]) * int(string1[2]))
+    if a == int(string1[4]):
+        print(True)
+    else:
+        print(False)
+
+elif string1[1] == "/":
+    a = (int(string1[0]) * int(string1[2]))
+    if a == int(string1[4]):
+        print(True)
+    else:
+        print(False)
+
+elif string1[1] == "+":
+    a = (int(string1[0]) * int(string1[2]))
+    if a == int(string1[4]):
+        print(True)
+    else:
+        print(False)
+
+elif string1[1] == "-":
+    a = (int(string1[0]) * int(string1[2]))
+    if a == int(string1[4]):
+        print(True)
+    else:
+        print(False)
