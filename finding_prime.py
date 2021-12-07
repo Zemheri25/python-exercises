a844bff3bac73bf105acbb40ff44343d126c6d11
Zemheri25 <omerzemheri14@gmail.com>
2021-12-07 16:58:04 +0300

    I added fucntion

--- finding_prime.py
+++ finding_prime.py
@@ -8,7 +8,18 @@ def prime_number(number):
 list1 = []
 number1 = int(input("Please enter your number : "))
 for i in range(number1, 100):
-    if prime_number(i):
+    if prime_number(i):                                     #THİS SOLUTİON WİTH NORMAL METHODS
         list1.append(i)
 
 print(min(list1))
+
+def next_prime(number1):
+    number1 = int(input("Please enter your number : "))
+    list1 = []
+    for i in range(number1, 100):
+        if prime_number(i):                               #THİS SOLUTİON WİTH FUNCTİON
+            list1.append(i)
+
+    return min(list1)
+
+print(next_prime("number"))
