f1f3e9a530e76be2f04389535c8aeec97364ce02
Zemheri25 <omerzemheri14@gmail.com>
2021-12-10 01:43:24 +0300

    wrapping sentences!

---
+++ wrapping_sentence.py
@@ -0,0 +1,9 @@
+def wrap_around(string1, number1):
+    string1 = input("Please enter your string1 : ")
+    number1 = int(input("Please enter your number1 : "))
+    if number1 > 0:
+        return string1[number1-1:] + string1[:number1]
+    else:
+        return string1[-1:number1:-1][::-1] + string1[:number1]
+
+print(wrap_around("string1", "number1"))
