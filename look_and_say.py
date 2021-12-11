06336faa672aa76b1786c2381d3d9ac07f2dcd17
Zemheri25 <omerzemheri14@gmail.com>
2021-12-11 15:11:08 +0300

    sql command!

--- look_and_say.py
+++ look_and_say.py
@@ -1,31 +1,30 @@
-number = int(input("Please enter your number : "))
-number = str(number)
-count1 = 0
-count2 = 1
-str1 = ""
-if len(number) % 2 == 0 and len(number) != 0:
-    for i in range(int(len(number) / 2)):
-        str1 += int(number[count1]) * number[count2]      #THIS SOLUTİON WİTH NORMAL METHODS
-        count1 += 2
-        count2 += 2
-    print(str1)
-else:
-    print("invalid")
+# number = int(input("Please enter your number : "))
+# number = str(number)
+# count1 = 0
+# count2 = 1
+# str1 = ""
+# if len(number) % 2 == 0 and len(number) != 0:
+#     for i in range(int(len(number) / 2)):
+#         str1 += int(number[count1]) * number[count2]      #THIS SOLUTİON WİTH NORMAL METHODS
+#         count1 += 2
+#         count2 += 2
+#     print(str1)
+# else:
+#     print("invalid")
 
 
 
 def look_and_say(number):
-    number = input("Please enter your number : ")
     count1 = 0
     count2 = 1
     str1 = ""
     if len(number) % 2 == 0 and len(number) != 0:
         for i in range(int(len(number) / 2)):
-            str1 += int(number[count1]) * number[count2]     #THIS SOLUTİON WİTH FUNCTİON 
+            str1 += int(number[count1]) * number[count2]                                          #THIS SOLUTİON WİTH FUNCTİON 
             count1 += 2
             count2 += 2
         return str1
     else:
         return "invalid"
 
-print(look_and_say("number1"))
+print(look_and_say("1213141516171819"))
